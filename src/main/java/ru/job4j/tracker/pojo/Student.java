edbd76e770d1e42e106236d0b8981b5eff043ed0
denkrasn <den.job4j@yandex.ru>
2020-07-21 10:38:37 +0300

    2. Модель данных. [#299572]

---
+++ src/main/java/ru/job4j/tracker/pojo/Student.java
@@ -0,0 +1,49 @@
+package ru.job4j.tracker.pojo;
+
+public class Student {
+    private String surname;
+    private String name;
+    private String middleName;
+    private String group;
+    private String dateOfReceipt;
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getDateOfReceipt() {
+        return dateOfReceipt;
+    }
+
+    public void setDateOfReceipt(String dateOfReceipt) {
+        this.dateOfReceipt = dateOfReceipt;
+    }
+}
---
+++ src/main/java/ru/job4j/tracker/pojo/pojo/College.java
@@ -0,0 +1,17 @@
+package ru.job4j.tracker.pojo.pojo;
+
+import ru.job4j.tracker.pojo.Student;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setSurname("Красноштан");
+        student.setName("Денис");
+        student.setMiddleName("Константинович");
+        student.setGroup("7Ю");
+        student.setDateOfReceipt("01.09.2009");
+        System.out.println("ФИО: " + student.getSurname() + " " + student.getName() + " " + student.getMiddleName());
+        System.out.println("Группа: " + student.getGroup());
+        System.out.println("Дата поступления: " + student.getDateOfReceipt());
+    }
+}
